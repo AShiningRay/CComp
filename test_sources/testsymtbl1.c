f4cb4f02a9409e5a4dc3598be14fd53b8651a4e0
AShiningRay <genesis45.ppc@gmail.com>
2022-11-27 12:40:06 -0300

    Add a small test to check the symbol table.

---
+++ test_sources/testsymtbl1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int a;
+    int b=1;
+    int c=2;
+    int d=3;
+    if(d < 2) 
+    {
+        printf("Something's wrong here...\n");
+    }
+    else
+    {
+        printf("Hello World!\n");
+    }
+
+    a = 12;
+    b = 5;
+}
