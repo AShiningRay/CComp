d6a64d46428bb33330399321dc7fcfd4907af03a
AShiningRay <genesis45.ppc@gmail.com>
2022-11-28 22:16:28 -0300

    Add more tests to help debug global vars and struct support.
    We still need to implement rules for those, so having those tests
    at the ready will prove useful.

---
+++ test_sources/test5_globalvars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int glob1;
+char glob2 = 'g';
+float glob3 = 2.53;
+
+int main ()
+{
+
+	printf("Hello World");
+	
+	return 0;
+}
---
+++ test_sources/test6_structs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct person 
+{
+	int age = 45;
+	char first_letter = 'D';
+	float weight: 65.4;
+};
+
+int main ()
+{
+	printf("Hello World");
+	
+	return 0;
+}
